592314e1ddf2c78db982c8bbbaffba6cc5ba2eb3
Adrian Ramos Marquez <adrianteeeen@gmail.com>
2026-07-27 22:55:35 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -20,14 +20,14 @@ if (cursor && cursorBlur) {
             const isLight = localStorage.getItem('theme') === 'light';
             cursor.style.width = '20px';
             cursor.style.height = '20px';
-            cursor.style.backgroundColor = isLight ? '#0e850b' : '#6ff066';
+            cursor.style.backgroundColor = isLight ? '#580da0' : '#c084fc';
         });
 
         el.addEventListener('mouseleave', () => {
             const isLight = localStorage.getItem('theme') === 'light';
             cursor.style.width = '10px';
             cursor.style.height = '10px';
-            cursor.style.backgroundColor = isLight ? '#084d06' : '#40e437';
+            cursor.style.backgroundColor = isLight ? '#460a7f' : '#a855f7';
         });
     });
 }
@@ -76,9 +76,9 @@ function copyEmail(button) {
             button.innerText = "¡Email Copiado!";
         }
         
-        button.style.borderColor = "#40e437";
+        button.style.borderColor = "#a855f7";
         button.style.color = "#000000";             // <- Texto en negro
-        button.style.backgroundColor = "#40e437";
+        button.style.backgroundColor = "#a855f7";
 
         setTimeout(() => {
             if (copyTextElement) {
